d63f67057fd0b50cae5f95c395f3de14f4f186b5
Alexander Govea <alexander_alatorre1993@hotmail.com>
2019-08-29 16:12:50 -0700

    added dynamic rendering of data

--- index.js
+++ index.js
@@ -31,17 +31,62 @@ fs.writeFileSync('./txt/output.txt', textOut);
     //this will send out a response as requested
 
 //top level code is only executed once it starts the program
+    //product.productName is how we grab the json object from dev-data/data.json
+const replaceTemplate = (template, product) => {
+    let output = template.replace(/{%PRODUCT_NAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
+
+const tempOverview = fs.readFileSync(
+    `${__dirname}/templates/template-overview.html`,
+    'utf-8'
+  );
+
+const tempCard = fs.readFileSync(
+    `${__dirname}/templates/template-card.html`,
+    'utf-8'
+);
+
+const tempProduct = fs.readFileSync(
+    `${__dirname}/templates/template-product.html`,
+    'utf-8'
+);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+
+
 const dataObj = JSON.parse(data);
+
+//READS THE TEMPLATE OVEVIEW
 
 const server = http.createServer((req, res) => {
     const pathname = req.url;
 
+    //OVERVIEW PAGE
+        //this will render the html template above
     if (pathname === '/' || pathname === '/overview') {
-        res.end('this is the overview')
+        res.writeHead(200, {'Content-type': 'text/html'});
+
+        const cardsHTML = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHTML);
+
+        res.end(output);
+
+    //PRODUCT PAGE
     } else if (pathname === '/product') {
         res.end('this is the product')
+
+
+    //API PAGE
     } else if (pathname === '/api') {
         ///this will read the file and send back the data
         fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
@@ -49,6 +94,9 @@ const server = http.createServer((req, res) => {
             res.writeHead(200, {'Content-type': 'application/json'});
             res.end(data);
         });
+
+
+    //NOT FOUND
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html'
